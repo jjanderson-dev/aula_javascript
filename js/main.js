de45f97b2354c3a23ee9033cb60a10d75726ff60
jjanderson-dev <jjanderson.rezende@gmail.com>
2020-02-26 15:42:54 -0300

    Aula inicial de javascript

---
+++ js/main.js
@@ -0,0 +1,82 @@
+function clicou(){
+    alert("Obrigado por clicar");
+}
+
+
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaidade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validade = false
+    }
+}
+
+var idade = prompt("Qual é a sua idade ?");
+console.log(validaidade(idade));
+
+aler
+
+*/
+
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+/*
+var d = new Date();
+alert(d.getMinutes());
+*/
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+    alert(count);
+}
+*/
+/*
+var idade = prompt("Qual é a sua idade ?");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+
+
+
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop("uva");
+
+
+//console.log(lista);
+
+//var nome = "Janderson Rezende";
+//var idade = 43;
+//
+//var idade2 = 20;
+//alert(nome + " tem " + idade +  " anos ");
